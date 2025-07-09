4981f3045040f8eca51dc18195b7d661856bfbe4
Hector <hectorjesus029@gmail.com>
2025-07-09 17:22:40 -0600

    Inicialización

---
+++ src/app/modules/home/home.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [CommonModule],
+  standalone: true,
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
