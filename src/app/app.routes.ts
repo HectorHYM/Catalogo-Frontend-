c3a32752f46deb0384538db657df3523f7b864c9
Hector <hectorjesus029@gmail.com>
2025-07-15 16:34:13 -0600

    Inicialización

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,4 +7,8 @@ export const routes: Routes = [
         component: HomeComponent,
         loadChildren: () => import('./modules/home/home.routes').then(m => m.HomeRoutes)
     }
+<<<<<<< HEAD
 ];
+=======
+];
+>>>>>>> 323555d (Inicialización)
