71937560c92a9dc69cab7c02943cec9c551b4507
Hector <hectorjesus029@gmail.com>
2025-07-15 18:34:01 -0600

    feat(footer): Implementación del componente del registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './modules/home/home.component';
+import { HomeComponent } from '@modules/home/home.component';
+import { RegisterComponent } from '@modules/register/register.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent,
         loadChildren: () => import('./modules/home/home.routes').then(m => m.HomeRoutes)
+    },
+    {
+        path: 'register',
+        component: RegisterComponent,
+        loadChildren: () => import('./modules/register/register.routes').then(m => m.RegisterRoutes)
     }
 ];
---
+++ src/app/modules/register/register.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  imports: [CommonModule],
+  standalone: true,
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+
+}
