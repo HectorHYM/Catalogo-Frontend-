7e518c8e1280640ea00314c46febd773ce79d736
Hector <hectorjesus029@gmail.com>
2025-10-06 16:31:56 -0600

    Modificación de obtención de parámetros de URL de QueryParams a Fragment y limpieza de URL al navegar en ella

--- src/app/modules/password/password.component.ts
+++ src/app/modules/password/password.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { GeneralResponse } from '@core/interfaces/generalResponse';
 import { UserService } from '@core/services/user.service';
 import { SnackbarService } from '@core/services/snackbar.service';
@@ -25,7 +25,7 @@ export class PasswordComponent implements OnInit{
   token : string | null = null;
   flow: 'set' | 'recover' | null = null;
 
-  constructor(fb : FormBuilder, private userSvc : UserService, private router : Router, private snackBarService : SnackbarService){
+  constructor(fb : FormBuilder, private userSvc : UserService, private router : Router, private snackBarService : SnackbarService, private route: ActivatedRoute){
     this.form = fb.group({
       password: ['', [Validators.required, Validators.minLength(8), this.hasLowerCase(), this.hasUpperCase(), this.hasSpecialChar(), this.hasNumber()]],
       repeat_password: ['', [Validators.required, Validators.minLength(8), Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).+$/)]]
@@ -82,9 +82,19 @@ export class PasswordComponent implements OnInit{
   get repeat_password() { return this.form.get('repeat_password')!; }
 
   ngOnInit(): void {
-    const urlParams = new URLSearchParams(window.location.search);
-    this.token = urlParams.get('token');
-    this.flow = urlParams.get('flow') as 'set' | 'recover' | null;
+    this.route.fragment.subscribe(frag => {
+      if(!frag) return;
+      const params = new URLSearchParams(frag);
+      this.token = params.get('token');
+      this.flow = params.get('flow') === 'set' ? 'set' : 'recover';
+
+      //^LOG
+      console.log("Token obtenido de la URL: ", this.token);
+      console.log("Flujo obtenido de la URL: ", this.flow);
+
+      const clean = window.location.pathname + window.location.search;
+      window.history.replaceState({}, document.title, clean);
+    });
   }
 
   //* Método para validar que las contraseñas coincidan
